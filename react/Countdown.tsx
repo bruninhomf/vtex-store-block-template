ce78ed7414a065bb85eb04bdba4ff156f0279e8e
Bruno <bruno.firmiano@acct.global>
2022-05-19 17:30:18 -0300

    Add layout components

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import * as React from 'react'
+import { InputLead } from '../src/components/Form/InputLead'
+import { ButtonLead } from '../src/components/Form/ButtonLead'
 
-interface CountdownProps {}
+interface CountdownProps { }
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({}) => {
-  return <div />
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
+  return (
+    <>
+      <form>
+        <InputLead
+          type='txt'
+          name='firstName'
+          text='Nome'
+          value=''
+          placeholder=''
+        />
+        <InputLead
+          type='txt'
+          name='lastName'
+          text='Sobre Nome'
+          value=''
+          placeholder=''
+        />
+        <InputLead
+          type='txt'
+          name='email'
+          text='E-mail'
+          value=''
+          placeholder=''
+        />
+        <InputLead
+          type='txt'
+          name='whatsapp'
+          text='WhatsApp'
+          value=''
+          placeholder=''
+        />
+        <ButtonLead
+          name='send'
+          type='button'
+          text='Salvar'
+        />
+      </form>
+    </>
+  )
 }
 
 Countdown.schema = {
