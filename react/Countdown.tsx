656328cbdd8318257edb3b770fc85beb24051a2a
Bruno <bruno.firmiano@acct.global>
2022-05-20 16:10:27 -0300

    Starting a new branch without components

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,47 +1,11 @@
 import * as React from 'react'
-import { InputLead } from '../src/components/Form/InputLead'
-import { ButtonLead } from '../src/components/Form/ButtonLead'
 
 interface CountdownProps { }
 
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
   return (
     <>
-      <form>
-        <InputLead
-          type='txt'
-          name='firstName'
-          text='Nome'
-          value=''
-          placeholder=''
-        />
-        <InputLead
-          type='txt'
-          name='lastName'
-          text='Sobre Nome'
-          value=''
-          placeholder=''
-        />
-        <InputLead
-          type='txt'
-          name='email'
-          text='E-mail'
-          value=''
-          placeholder=''
-        />
-        <InputLead
-          type='txt'
-          name='whatsapp'
-          text='WhatsApp'
-          value=''
-          placeholder=''
-        />
-        <ButtonLead
-          name='send'
-          type='button'
-          text='Salvar'
-        />
-      </form>
+      
     </>
   )
 }
